20c1ccd226f92ff9e445fb56c505991835bbf80c
Serdar Arici <serdararici3@gmail.com>
2025-10-04 13:55:32 +0300

    Add authentication middleware and login/register pages

---
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -0,0 +1,21 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function LoginPage() { 
+    return (
+        <main className="flex flex-col items-center p-5 gap10 animate-fade-in ">
+            <Image
+                src='/assets/logo.png'
+                alt='Kalenda Logo'
+                width={100}
+                height={100}
+            />
+
+            <div className="mt-3">
+                <SignIn/>
+                
+            </div>
+
+        </main>
+    )
+}
