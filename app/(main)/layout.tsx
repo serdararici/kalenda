ebecf136e8ff52b45bfe2d8aa1ab7108ad3b1975
Serdar Arici <serdararici3@gmail.com>
2025-10-06 11:31:30 +0300

    feat: implement layout and navigation components; add global styles and utility functions

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,26 @@
+
+import PublicNavBar from "@/components/PublicNavBar"
+import { currentUser } from "@clerk/nextjs/server"
+
+export default async function MainLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+
+    const user = await currentUser()
+
+    return (
+        <main className="relative">
+            <PublicNavBar />
+            {/* Render PrivateNavBar if user exists, otherwise PublicNavBar */}
+            {/* {user ? <PrivateNavBar /> : <PublicNavBar />} */}
+
+
+            {/* Render the children */}
+            <section className="pt-36 ">
+                {children}
+            </section>
+        </main>
+    )
+}
